ae11a90e9bab072b4a85e00a9ce4af6bc57a46f0
U-Khana-PC\Khana <Khana@Khana-PC.(none)>
2013-10-12 19:15:50 -0400

    Upgraded LCD_CLOCK to use class to update the clock

--- LCD_CLOCK/LCD_CLOCK.ino
+++ LCD_CLOCK/LCD_CLOCK.ino
@@ -1,7 +1,7 @@
 /*
     A working clock that resets when the plug is turned off.
  */
- 
+
 // include the library code:
 #include <LiquidCrystal.h>
 
@@ -9,72 +9,81 @@
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-int hour = 00;
-int minute = 00;
-int second = 00;
+class SIMPLECLOCK {
+    public:
+    int hour;
+    int minute;
+    int second;
+    void tick();
+    void checkTime();
+};
 
-void setup() {
-    // set up the LCD's number of columns and rows: 
-    lcd.begin(16, 2);
-    // Print a message to the LCD.
-    lcd.print("Amazing Clock!");
+void SIMPLECLOCK::tick () {
+    second += 1;
 }
 
-void loop() {
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    //  second = millis()/1000;
-    delay(1000);
-    second += 1;
+void SIMPLECLOCK::checkTime () {
     if (second == 60) {
         minute += 1;
         second = 0;
         if (minute == 60) {
             hour += 1;
             minute = 0;
-            //second = 0;
             if (hour == 24) {
                 hour = 0;
-                //minute = 0;
-                //second = 0;
             }
         }
     }
-          
+}
+
+SIMPLECLOCK simpleClock;
+
+void setup() {
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print("Ali's Clock!");
+}
+
+void loop() {
+
+    delay(1000);
+    simpleClock.tick();
+    simpleClock.checkTime();
     lcd.setCursor(0, 1);
-    if (hour >= 10) {    
-        lcd.print(hour);
+    if (simpleClock.hour >= 10) {
+        lcd.print(simpleClock.hour);
     }
     else {
         lcd.print("0");
         lcd.setCursor(1,1);
-        lcd.print(hour);
+        lcd.print(simpleClock.hour);
     }
 
     lcd.setCursor(2,1);
     lcd.print(":");
 
     lcd.setCursor(3,1);
-    if (minute >= 10) {    
-        lcd.print(minute);
+    if (simpleClock.minute >= 10) {
+        lcd.print(simpleClock.minute);
     }
     else {
         lcd.print("0");
         lcd.setCursor(4,1);
-        lcd.print(minute);
+        lcd.print(simpleClock.minute);
     }
 
     lcd.setCursor(5,1);
     lcd.print(":");
 
     lcd.setCursor(6,1);
-    if (second >= 10) {    
-        lcd.print(second);
+    if (simpleClock.second >= 10) {
+        lcd.print(simpleClock.second);
     }
     else {
         lcd.print("0");
         lcd.setCursor(7,1);
-        lcd.print(second);
+        lcd.print(simpleClock.second);
     }
 }
 
